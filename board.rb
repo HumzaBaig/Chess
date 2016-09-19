900b88de39ec74123b69176d2cb26dbb5092fb13
App Academy Student <student@appacademy.io>
2016-09-19 15:53:35 -0700

    Refactor, Create Board#move method

--- board.rb
+++ board.rb
@@ -1,28 +1,49 @@
 require_relative 'piece'
 require_relative 'null_piece'
 require 'byebug'
+
 class Board
+
   def initialize
-    @board = Array.new(8) { Array.new(8) }
+
     set_board
   end
 
   def set_board
     pieces = [0,1,6,7]
     # debugger
-    @board = @board.map.with_index do |row, index|
-      row.map do |pos|
-        if pieces.include?(index)
-          pos = Piece.new("Piece")
+    @board = Array.new(8) { Array.new(8) }
+    8.times do |row_index|
+      8.times do |col_index|
+        if pieces.include?(row_index)
+          @board[row_index][col_index] = Piece.new("Piece",[row_index, col_index])
         else
-          pos = NullPiece.new("NullPiece")
+          @board[row_index][col_index] = NullPiece.new("NullPiece")
         end
       end
     end
   end
 
+  def move(start, end_pos)
+    self[start], self[end_pos] = self[end_pos], self[start]
+    self[end_pos].update_position(end_pos)
+  end
+
+  def []=(pos,value)
+    row, col = pos
+    @board[row][col] = value
+  end
+
+  def [](pos)
+    row, col = pos
+    @board[row][col]
+  end
+
 end
 
 board = Board.new
+# p board[[0, 1]]
 
-p board
+a = [0,0]
+b = [3,4]
+p board.move(a,b)
--- piece.rb
+++ piece.rb
@@ -1,5 +1,22 @@
+# require_relative 'board'
+
 class Piece
-  def initialize(piece)
+  def initialize(piece, position)
     @piece = piece
+    @pos = position
+  end
+
+  def update_position(end_pos)
+    @pos = end_pos
+  end
+
+  def []=(pos,value)
+    row, col = pos
+    @pos[row][col] = value
+  end
+
+  def [](pos)
+    row, col = pos
+    @pos[row][col]
   end
 end
